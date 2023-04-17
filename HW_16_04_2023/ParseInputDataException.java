79ab5c71dc1a25d0af6090024b8165cc10540b14
MrBizzon <info.proect@mail.ru>
2023-04-17 08:34:24 +0300

    The final certification has been completed

---
+++ HW_16_04_2023/ParseInputDataException.java
@@ -0,0 +1,14 @@
+package HW_16_04_2023;
+
+public class ParseInputDataException extends Exception {
+    String data;
+
+    public ParseInputDataException(String inputData) {
+        this.data = inputData;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.data;
+    }
+}
